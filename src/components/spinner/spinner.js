4b2377c291b51d35e19f40c884a68f67f825fd3f
nikita <honeyqumo@gmail.com>
2022-02-12 17:49:00 +0300

    v.0.0.2 | Реализовал loader во время прогрузки компонента

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-double-ring-yspenyd33nr">
+            <div class="ldio-6jq4ek1njku">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
