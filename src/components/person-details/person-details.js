d7b23ad5307323daf3b55d9db5f9414d509a0fa6
nikita <honeyqumo@gmail.com>
2022-02-14 18:24:18 +0300

    v0.1.1 | Bugfix

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import './person-details.css';
 import SwapiService from "../../services/swapi-service";
+import Spinner from '../spinner';
 
 export default class PersonDetails extends Component {
 
